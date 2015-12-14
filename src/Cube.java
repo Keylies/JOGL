453a1663ca014c3cbd4d452e3d3082d269f7781a
Keylies <lemenct@gmail.com>
2015-12-14 15:46:38 +0100

    Texture mapping added

--- src/Cube.java
+++ src/Cube.java
@@ -1,16 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureIO;
 
 
 public class Cube implements Object3D {
 	
-	private float x, y, z, l; // Position et taille (taille : l)
+	private float x, y, z, l; // Position et taille
 	private float tx, ty, tz; // Angle
 	private float speedx = 0;
 	private float speedy = 0;
 	private float speedz = 0;
+	private float orbx, orby, orbz;
+	private int texture;
 	
-	public Cube(float x, float y, float z, float l, float tx, float ty, float tz) {
+	public Cube(float x, float y, float z, float l, float tx, float ty, float tz, float ox, float oy, float oz, int t) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
@@ -18,68 +25,69 @@ public class Cube implements Object3D {
 		this.tx = tx;
 		this.ty = ty;
 		this.tz = tz;
+		orbx = ox;
+		orby = oy;
+		orbz = oz;
+		texture = t;
 	}
 	
 	public void render(GLAutoDrawable drawable) {
 		
 		GL2 gl = drawable.getGL().getGL2();
 		
-		gl.glLoadIdentity();
+
 		
 		gl.glPushMatrix();
 		
-		gl.glTranslatef(x, y, z);
-		
-		gl.glRotatef(speedx, 1.0f, 0.0f, 0.0f);
-		gl.glRotatef(speedy, 0.0f, 1.0f, 0.0f);
-		gl.glRotatef(speedz, 0.0f, 0.0f, 1.0f);
-		
-		gl.glBegin(gl.GL_QUADS);
+			gl.glTranslatef(x, y, z);
+			
+			gl.glRotatef(speedx, 1.0f, 0.0f, 0.0f);
+			gl.glRotatef(speedy, 0.0f, 1.0f, 0.0f);
+			gl.glRotatef(speedz, 0.0f, 0.0f, 1.0f);
+				
+			gl.glTranslatef(orbx, orby, orbz);
+			
+			gl.glBindTexture(gl.GL_TEXTURE_2D, texture);
+	
+			gl.glBegin(gl.GL_QUADS);
 
-			// FRONT
-			gl.glColor3f( 1.0f, 0.0f, 0.0f); //face rouge
-			gl.glVertex3f( -l, -l, -l);
-			gl.glVertex3f( l, -l, -l);
-			gl.glVertex3f( l, l, -l);
-			gl.glVertex3f( -l, l, -l);
-			
-			
-			// RIGHT
-			gl.glColor3f( 0.0f, 1.0f, 0.0f); //face verte
-			gl.glVertex3f( l, -l, -l);
-			gl.glVertex3f( l, -l, l);
-			gl.glVertex3f( l, l, l);
-			gl.glVertex3f( l, l, -l);
-			                                                                                                    
-			// BACK
-			gl.glColor3f( 1.0f, 0.0f, 0.0f); //face rouge
-			gl.glVertex3f( -l, -l, l);
-			gl.glVertex3f( l, -l, l);
-			gl.glVertex3f( l, l, l);
-			gl.glVertex3f( -l, l, l);
-			
-			// LEFT
-			gl.glColor3f( 0.0f, 1.0f, 0.0f); //face verte
-			gl.glVertex3f( -l, -l, l);
-			gl.glVertex3f( -l, -l, -l);
-			gl.glVertex3f( -l, l, -l);
-			gl.glVertex3f( -l, l, l);
-			
-			// TOP
-			gl.glColor3f( 0.0f, 0.0f, 1.0f); //face bleue
-			gl.glVertex3f( -l, l, -l);
-			gl.glVertex3f( l, l, -l);
-			gl.glVertex3f( l, l, l);
-			gl.glVertex3f( -l, l, l);
-			
-			// BOTTOM
-			gl.glColor3f( 0.0f, 0.0f, 1.0f); //face bleue
-			gl.glVertex3f( -l, -l, -l);
-			gl.glVertex3f( l, -l, -l);
-			gl.glVertex3f( l, -l, l);
-			gl.glVertex3f( -l, -l, l);
-
-		gl.glEnd();
+				// FRONT
+				gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( -l, -l, -l);
+				gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( l, -l, -l);
+				gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( l, l, -l);
+				gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( -l, l, -l);
+				
+				// RIGHT
+				gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( l, -l, -l);
+				gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( l, -l, l);
+				gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( l, l, l);
+				gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( l, l, -l);
+				                                                                                                    
+				// BACK
+				gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( -l, -l, l);
+				gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( l, -l, l);
+				gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( l, l, l);
+				gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( -l, l, l);
+				
+				// LEFT
+				gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( -l, -l, l);
+				gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( -l, -l, -l);
+				gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( -l, l, -l);
+				gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( -l, l, l);
+				
+				// TOP
+				gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( -l, l, -l);
+				gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( l, l, -l);
+				gl.glTexCoord2f(1.0f, 1.0f); gl.glVertex3f( l, l, l);
+				gl.glTexCoord2f(0.0f, 1.0f); gl.glVertex3f( -l, l, l);
+				
+				// BOTTOM
+				gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( -l, -l, -l);
+				gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( l, -l, -l);
+				gl.glTexCoord2f(1.0f, 0.0f); gl.glVertex3f( l, -l, l);
+				gl.glTexCoord2f(0.0f, 0.0f); gl.glVertex3f( -l, -l, l);
+	
+			gl.glEnd();
 		
 		gl.glPopMatrix();
 		
--- src/Render3D.java
+++ src/Render3D.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -5,22 +7,34 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.TextureIO;
 
 
 public class Render3D implements GLEventListener {
 	
 	private ArrayList<Object3D> objects = new ArrayList<Object3D>();
 	
-	private Object3D cube1 = new Cube(0.0f, 0.0f, -8.0f, 0.5f, 0.0f, 0.5f, 0.0f);
-	private Object3D cube2 = new Cube(0.0f, 1.0f, -8.0f, 0.2f, 0.0f, 0.1f, 0.0f);
-	private Object3D cube3 = new Cube(0.0f, 2.0f, -8.0f, 0.2f, 0.0f, 0.1f, 0.0f);
+	private Object3D cube1;
+	private Object3D cube2;
+	private Object3D cube3;
+	
+	File fileImg1;
+	File fileImg2;
+	File fileImg3;
+	Texture t1 = null;
+	Texture t2 = null;
+	Texture t3 = null;
+	int texture1;
+	int texture2;
+	int texture3;
 	
 	@Override
 	public void display(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
 		
 		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
-		//gl.glLoadIdentity();
+		gl.glLoadIdentity();
 		
 		update();
 		render(drawable);
@@ -36,15 +50,34 @@ public class Render3D implements GLEventListener {
 		GL2 gl = drawable.getGL().getGL2();
 		
 		gl.glShadeModel(gl.GL_LINE_SMOOTH);
-		
-		// Gestion profondeur
 		gl.glClearDepth(1.0f);
 		gl.glEnable(gl.GL_DEPTH_TEST);
 		gl.glDepthFunc(gl.GL_LEQUAL);
 		gl.glHint(gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST);
 
 		gl.glMatrixMode(gl.GL_MODELVIEW);
-		gl.glLoadIdentity();
+		
+		gl.glEnable(gl.GL_TEXTURE_2D);
+		
+		try {
+			fileImg1 = new File("t1.jpg");
+			fileImg2 = new File("t2.jpg");
+			fileImg3 = new File("t3.jpg");
+			t1 = TextureIO.newTexture(fileImg1, true);
+			t2 = TextureIO.newTexture(fileImg2, true);
+			t3 = TextureIO.newTexture(fileImg3, true);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		texture1 = t1.getTextureObject(gl);
+		texture2 = t2.getTextureObject(gl);
+		texture3 = t3.getTextureObject(gl);
+		
+		cube1 = new Cube(0.0f, 0.0f, -8.0f, 0.5f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, texture1);
+		cube2 = new Cube(0.0f, 1.0f, -8.0f, 0.2f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, texture2);
+		cube3 = new Cube(0.0f, 0.0f, -8.0f, 0.2f, 0.0f, 0.0f, 0.5f, 2.0f, 0.0f, 0.0f, texture3);
 		
 		objects.add(cube1);
 		objects.add(cube2);
@@ -72,9 +105,6 @@ public class Render3D implements GLEventListener {
 	
 	private void render(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		
-		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
-		gl.glLoadIdentity();
 		
 		for ( Object3D object : objects)
 			object.render(drawable);
